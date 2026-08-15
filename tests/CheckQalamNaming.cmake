9fc3cbc9713cf0f6d3f1c27271bb1077b78a2dc9
Omar Aglan <omar.aglan91@yahoo.com>
2026-08-15 12:28:09 +0300

    test: enforce Qalam naming guard

--- tests/CheckQalamNaming.cmake
+++ tests/CheckQalamNaming.cmake
@@ -10,17 +10,17 @@ file(GLOB_RECURSE qalam_owned_sources LIST_DIRECTORIES false
 
 foreach(source IN LISTS qalam_owned_sources)
     get_filename_component(name "${source}" NAME)
-    if(name MATCHES "^T[A-Z].*\\.(cpp|h)$")
+    if("${name}" MATCHES "^T[A-Z].*\\.(cpp|h)$")
         message(FATAL_ERROR
             "Legacy T-prefixed Qalam source file is forbidden: ${source}")
     endif()
 
     file(READ "${source}" contents)
-    if(contents MATCHES "(class|struct)[ \t\r\n]+T[A-Z]")
+    if("${contents}" MATCHES "(class|struct)[ \t\r\n]+T[A-Z]")
         message(FATAL_ERROR
             "Legacy T-prefixed Qalam type is forbidden: ${source}")
     endif()
-    if(contents MATCHES "#[ \t]*include[ \t]+\"T[A-Z]")
+    if("${contents}" MATCHES "#[ \t]*include[ \t]+\"T[A-Z]")
         message(FATAL_ERROR
             "Legacy T-prefixed Qalam include is forbidden: ${source}")
     endif()
