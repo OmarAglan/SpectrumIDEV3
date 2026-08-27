e660a1625bb2a97a63792086570e3638b3c425c0
Omar Aglan <omar.aglan91@yahoo.com>
2026-08-27 12:18:42 +0300

    Verify one panel across editor groups

--- tests/TestEditorWorkspace.cpp
+++ tests/TestEditorWorkspace.cpp
@@ -1,9 +1,13 @@
 #include "FileManager.h"
+#include "LayoutManager.h"
 #include "QalamDocumentModel.h"
 #include "QalamEditor.h"
 #include "QalamEditorWorkspace.h"
+#include "QalamPanelArea.h"
+#include "QalamSearchPanel.h"
 
 #include <QFile>
+#include <QMainWindow>
 #include <QTabWidget>
 #include <QTemporaryDir>
 #include <QTest>
@@ -16,6 +20,7 @@ class TestEditorWorkspace final : public QObject
 private slots:
     void sharesContentPathAndUndoAcrossTwoGroups();
     void movesTabsAndNeverCreatesMoreThanTwoGroups();
+    void keepsOneSharedBottomPanelAcrossEditorGroups();
 };
 
 void TestEditorWorkspace::sharesContentPathAndUndoAcrossTwoGroups()
@@ -91,6 +96,23 @@ void TestEditorWorkspace::movesTabsAndNeverCreatesMoreThanTwoGroups()
     workspace.closeSecondaryGroup();
     QCOMPARE(workspace.groupCount(), 1);
     QCOMPARE(workspace.count(), 3);
+}
+
+void TestEditorWorkspace::keepsOneSharedBottomPanelAcrossEditorGroups()
+{
+    QMainWindow window;
+    QalamEditorWorkspace workspace;
+    QalamSearchPanel searchPanel;
+    LayoutManager layoutManager(&window, &workspace, &searchPanel, &window);
+    layoutManager.setupLayout();
+
+    auto *editor = new QalamEditor(&workspace);
+    workspace.addTab(editor, QStringLiteral("لوحة مشتركة.باء"));
+    QVERIFY(workspace.splitCurrent(Qt::Horizontal));
+    QCOMPARE(workspace.groupCount(), 2);
+    QCOMPARE(window.findChildren<QalamPanelArea*>().size(), 1);
+    QCOMPARE(layoutManager.panelArea(),
+             window.findChild<QalamPanelArea*>());
 }
 
 QTEST_MAIN(TestEditorWorkspace)
